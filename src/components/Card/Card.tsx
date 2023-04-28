40cabb448121a4f7ba8bcf4304a24de65b4d2c0e
Bumble <bumble.sakh@gmail.com>
2023-04-29 02:55:47 +1100

    feat: add column component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,15 @@
 import styles from './Card.module.scss';
+import data from '../../data';
+import Column from '../Column';
 
 const Card = () => {
-  return <div className={styles.card}></div>;
+  return (
+    <div className={styles.card}>
+      {data.map((column) => (
+        <Column key={column.header} icon={column.icon} header={column.header} text={column.text} color={column.color} />
+      ))}
+    </div>
+  );
 };
 
 export default Card;
---
+++ src/components/Column/Column.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import styles from './Column.module.scss';
+
+type ColumnProps = {
+  icon: string;
+  header: string;
+  text: string;
+  color: string;
+};
+
+const Column: FC<ColumnProps> = ({ icon, header, text, color }) => {
+  return (
+    <div className={`${styles.column} ${styles[color]}`}>
+      <div className={styles.icon}>
+        <img src={icon} alt='icon' />
+      </div>
+      <h2 className={styles.header}>{header}</h2>
+      <p className={styles.text}>{}text</p>
+      <button className={styles.btn}>Learn More</button>
+    </div>
+  );
+};
+
+export default Column;
