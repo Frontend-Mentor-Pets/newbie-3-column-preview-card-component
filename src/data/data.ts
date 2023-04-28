7915d34c23821a85cd9f7ca9c72c16d419dba828
Bumble <bumble.sakh@gmail.com>
2023-04-29 02:55:23 +1100

    feat: add data

---
+++ src/data/data.ts
@@ -0,0 +1,26 @@
+import sedan from '../assets/images/icon-sedans.svg';
+import suv from '../assets/images/icon-suvs.svg';
+import luxury from '../assets/images/icon-luxury.svg';
+
+const data = [
+  {
+    icon: sedan,
+    header: 'Sedans',
+    text: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+    color: 'bright-orange',
+  },
+  {
+    icon: suv,
+    header: 'SUVs',
+    text: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+    color: 'dark-cyan',
+  },
+  {
+    icon: luxury,
+    header: 'Luxury',
+    text: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+    color: 'very-dark-cyan',
+  },
+];
+
+export default data;
